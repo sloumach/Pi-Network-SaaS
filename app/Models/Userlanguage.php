e843736b13d166481bd477e183da682f45f5cbdf
sloumach <insloumach@gmail.com>
2024-03-15 18:26:21 +0100

    seed languages DB

---
+++ app/Models/Userlanguage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Userlanguage extends Model
+{
+    use HasFactory;
+}
